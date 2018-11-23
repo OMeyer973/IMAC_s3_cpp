7395573101cea08232699b845b92ff9eb5abedb0
Olivier Meyer <olivier.meyer973@gmail.com>
2018-11-23 11:39:54 +0100

    TD8 added stuff

--- TP8_templates/ex2/Point3D.hpp
+++ TP8_templates/ex2/Point3D.hpp
@@ -13,27 +13,49 @@ template<typename Type>
 class Point3D {
 
 public:
-	///\brief constructor
+	///\brief default constructor
 	inline Point3D()
 	:_x(0), _y(0), _z(0)
 	{};
 	
+	///\brief default constructor
 	Point3D(const Type& x, const Type& y, const Type& z)
 	:_x(x), _y(y), _z(z)
 	{};
 	
+	///\brief copy constructor
 	Point3D(const Point3D<Type>& p)
 	:_x(p._x), _y(p._y), _z(p._z)
+	{};
+
+	///\brief copy constructor w template
+	template<typename U>
+	Point3D(const Point3D<U>& p) const
+	:_x(p.getX()), _y(p.getY()), _z(p.getZ())
 	{};
 
 	///\brief destructor
 	inline ~Point3D(){};
 
-
 	///\brief getters
 	inline Type getX() const { return _x; };
 	inline Type getY() const { return _y; };
 	inline Type getZ() const { return _z; };
+
+	///\brief add a vec and a scalar
+	inline Point3D operator+ (const Type& t) const {
+		return Point3D(_x+t, _y+t, _z+t);
+	};
+	
+	///\brief add 2 vec
+	inline Point3D operator+ (const Point3D<Type>& p) const {
+		return Point3D(_x+p._x, _y+p._y, _z+p._y);
+	};
+
+	///\brief display with cout << ... <<
+	friend std::ostream &operator<<(std::ostream &os, const Point3D &p) {
+		return os << "[" << p.getX() << ", " << p.getY() << ", " << p.getZ() << "]";
+	}
 
 
 protected:
--- TP8_templates/ex2/main.cpp
+++ TP8_templates/ex2/main.cpp
@@ -10,6 +10,16 @@ int main(int argc, char const *argv[]) {
 	Point3D<int> p2(1,2,3);
 	Point3D<int> p3(p1);
 	
+	std::cout << p2 <<std::endl;
+	std::cout << p3 <<std::endl;
+	
+	Point3D<int> p4 = p1 + 1;
+	std::cout << p4 <<std::endl;
+
+	Point3D<int> p5 = p2 + p4;
+	std::cout << p5 <<std::endl;
+	
+	Point3D<float> p6(p5);
 	return 0;
 }
 
